c40e548198daa06e192f6c0160b219e209d7660a
Marcelo Munhoz <me@marcelomunhoz.com>
2021-08-21 21:45:03 -0300

    Wireframe and navigation layout

---
+++ src/components/Navigation.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Vuetify Todo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            A simple & fast todo list
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in items" :key="item.title" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-format-list-checks" },
+      { title: "About", icon: "mdi-help-box" },
+    ],
+  }),
+};
+</script>
