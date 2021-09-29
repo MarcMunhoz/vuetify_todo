2bfeb1a583fd3b012db58b3b8ae7d461d59fbf9f
Marcelo Munhoz <me@marcelomunhoz.com>
2021-09-29 11:08:27 -0300

    [New feature] Sort list

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -4,79 +4,83 @@
     <v-text-field outlined label="Add task" clearable hide-details v-model="newTaskTitle" @click:append="addTask" @keyup.enter="addTask" append-icon="mdi-plus-circle" class="mb-6"></v-text-field>
 
     <v-list flat class="pt-0">
-      <!-- Looping for the tasks -->
-      <div v-for="(task, i) in tasks" :key="task.id" @click="doneTask(task.id)">
-        <v-list-item :class="{ 'blue lighten-5': task.done }">
-          <template v-slot:default>
-            <v-list-item-action>
-              <v-checkbox :input-value="task.done"></v-checkbox>
-            </v-list-item-action>
+      <draggable v-model="tasks" ghost-class="ghost">
+        <transition-group type="transition" name="flip-list">
+          <!-- Looping for the tasks -->
+          <div class="sortable" v-for="(task, i) in tasks" :key="task.id" @click="doneTask(task.id)">
+            <v-list-item :class="{ 'blue lighten-5': task.done }">
+              <template v-slot:default>
+                <v-list-item-action>
+                  <v-checkbox :input-value="task.done"></v-checkbox>
+                </v-list-item-action>
 
-            <v-list-item-content>
-              <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title }}</v-list-item-title>
-            </v-list-item-content>
+                <v-list-item-content>
+                  <v-list-item-title :class="{ 'text-decoration-line-through': task.done }">{{ task.title }}</v-list-item-title>
+                </v-list-item-content>
 
-            <v-list-item-content v-if="task.dueDate" class="text-right">
-              <v-list-item-title class="caption"><v-icon dense class="mr-1">mdi-calendar-outline</v-icon>{{ computedDue(task.dueDate) }}</v-list-item-title>
-            </v-list-item-content>
+                <v-list-item-content v-if="task.dueDate" class="text-right">
+                  <v-list-item-title class="caption"><v-icon dense class="mr-1">mdi-calendar-outline</v-icon>{{ computedDue(task.dueDate) }}</v-list-item-title>
+                </v-list-item-content>
 
-            <v-menu>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn dark icon v-bind="attrs" v-on="on">
-                  <v-icon color="primary lighten-1">mdi-dots-vertical</v-icon>
-                </v-btn>
-              </template>
-
-              <v-list>
-                <v-list-item>
-                  <v-list-item-title>
-                    <div role="button" @click="(task.modal = true), (dialog = 0)">
-                      <v-btn icon>
-                        <v-icon color="grey">mdi-pencil</v-icon>
-                      </v-btn>
-                      Edit
-                    </div>
-                  </v-list-item-title>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-title>
-                    <div role="button" @click="(task.modal = true), (dialog = 1)">
-                      <v-btn icon>
-                        <v-icon color="grey">mdi-calendar-clock</v-icon>
-                      </v-btn>
-                      Due Date
-                    </div>
-                  </v-list-item-title>
-                </v-list-item>
-                <v-list-item v-for="(item, i) in sideMenu" :key="i">
-                  <v-list-item-title role="button" @click.stop="handleFnCall(item.function, task.id)">
-                    <v-btn icon>
-                      <v-icon color="grey">mdi-{{ item.button }}</v-icon>
+                <v-menu>
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-btn dark icon v-bind="attrs" v-on="on">
+                      <v-icon color="primary lighten-1">mdi-dots-vertical</v-icon>
                     </v-btn>
-                    {{ item.title }}
-                  </v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </template>
-        </v-list-item>
-        <v-divider></v-divider>
-        <v-dialog ref="dialog" v-if="dialog === 0" v-model="task.modal" :return-value.sync="task.title" persistent width="290px">
-          <v-card>
-            <v-card-title>Edit task</v-card-title>
-            <v-text-field v-model="task.title" @keyup.enter="$refs.dialog[i].save(task.title), snackBar('Task updated!')" class="pa-5"></v-text-field>
-            <v-btn text color="primary" @click.stop="task.modal = false">Cancel</v-btn>
-            <v-btn text color="primary" @click.stop="$refs.dialog[i].save(task.title), snackBar('Task updated!')">Save</v-btn>
-          </v-card>
-        </v-dialog>
+                  </template>
 
-        <v-dialog ref="dialog" v-else v-model="task.modal" :return-value.sync="task.dueDate" persistent width="290px">
-          <v-date-picker v-model="task.dueDate" scrollable :min="today()">
-            <v-btn text color="primary" @click.stop="task.modal = false">Cancel</v-btn>
-            <v-btn text color="primary" @click.stop="$refs.dialog[i].save(task.dueDate), snackBar('Due date is setted!')">OK</v-btn>
-          </v-date-picker>
-        </v-dialog>
-      </div>
+                  <v-list>
+                    <v-list-item>
+                      <v-list-item-title>
+                        <div role="button" @click="(task.modal = true), (dialog = 0)">
+                          <v-btn icon>
+                            <v-icon color="grey">mdi-pencil</v-icon>
+                          </v-btn>
+                          Edit
+                        </div>
+                      </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                      <v-list-item-title>
+                        <div role="button" @click="(task.modal = true), (dialog = 1)">
+                          <v-btn icon>
+                            <v-icon color="grey">mdi-calendar-clock</v-icon>
+                          </v-btn>
+                          Due Date
+                        </div>
+                      </v-list-item-title>
+                    </v-list-item>
+                    <v-list-item v-for="(item, i) in sideMenu" :key="i">
+                      <v-list-item-title role="button" v-if="(item.title = 'Delete')" @click.stop="handleFnCall(item.function, task.id)">
+                        <v-btn icon>
+                          <v-icon color="grey">mdi-{{ item.button }}</v-icon>
+                        </v-btn>
+                        {{ item.title }}
+                      </v-list-item-title>
+                    </v-list-item>
+                  </v-list>
+                </v-menu>
+              </template>
+            </v-list-item>
+            <v-divider></v-divider>
+            <v-dialog ref="dialog" v-if="dialog === 0" v-model="task.modal" :return-value.sync="task.title" persistent width="290px">
+              <v-card>
+                <v-card-title>Edit task</v-card-title>
+                <v-text-field v-model="task.title" @keyup.enter="$refs.dialog[i].save(task.title), snackBar('Task updated!')" class="pa-5"></v-text-field>
+                <v-btn text color="primary" @click.stop="task.modal = false">Cancel</v-btn>
+                <v-btn text color="primary" @click.stop="$refs.dialog[i].save(task.title), snackBar('Task updated!')">Save</v-btn>
+              </v-card>
+            </v-dialog>
+
+            <v-dialog ref="dialog" v-else v-model="task.modal" :return-value.sync="task.dueDate" persistent width="290px">
+              <v-date-picker v-model="task.dueDate" scrollable :min="today()">
+                <v-btn text color="primary" @click.stop="task.modal = false">Cancel</v-btn>
+                <v-btn text color="primary" @click.stop="$refs.dialog[i].save(task.dueDate), snackBar('Due date is setted!')">OK</v-btn>
+              </v-date-picker>
+            </v-dialog>
+          </div>
+        </transition-group>
+      </draggable>
     </v-list>
 
     <div v-if="tasks.length === 0" class="my-auto text-center green--text">
@@ -95,8 +99,13 @@
 </template>
 
 <script>
+import draggable from "vuedraggable";
+
 export default {
   name: "Todo",
+  components: {
+    draggable,
+  },
   data() {
     return {
       newTaskTitle: "",
@@ -115,9 +124,6 @@ export default {
     }
   },
   methods: {
-    dialogVal() {
-      this.dialog === 0 ? "task.title" : "task.dueDate";
-    },
     today() {
       // Gets the current date and returns "XXXX-XX-XX"
       const rawToday = new Date();
@@ -183,3 +189,23 @@ export default {
   },
 };
 </script>
+
+<style lang="less">
+.sortable {
+  cursor: move;
+
+  &-drag {
+    opacity: 0;
+  }
+}
+
+.flip-list-move {
+  transition: transform 0.5s;
+}
+
+.ghost {
+  border-left: 4px solid #90caf9;
+  box-shadow: 10px 10px 5px -1px hsla(0, 0, 0, 0.14);
+  opacity: 0.7;
+}
+</style>
